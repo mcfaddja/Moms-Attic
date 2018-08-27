94efa2b3e81f39bb429b39d6038e926ffb35ce13
Jonathan McFadden <mcfaddja@uw.edu>
2018-08-27 16:50:14 -0700

    more work on the last temp/relH sensor test sketch

--- TempRelH-tests/dht11_test3a/dht11_test3a.ino
+++ TempRelH-tests/dht11_test3a/dht11_test3a.ino
@@ -9,10 +9,24 @@
 //
 
 #include <dht.h>
+#include <Ethernet.h>
 
 dht DHT;
 
 #define DHT11_PIN 2
+
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+IPAddress ip(192, 168, 1, 250);
+
+EthernetServer server(80);
+
+
+double tempC = 0;
+double tempF = 0;
+double relH = 0;
+
 
 void setup()
 {
@@ -22,6 +36,24 @@ void setup()
   Serial.println(DHT_LIB_VERSION);
   Serial.println();
   Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)");
+
+  Ethernet.init(10);
+  Ethernet.begin(mac, ip);
+
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+
+  // start listening for clients
+  server.begin();
+  
+  delay(1000);
 }
 
 void loop()
@@ -44,13 +76,61 @@ void loop()
 		Serial.print("Unknown error,\t"); 
 		break;
   }
-  // DISPLAY DATA
-  Serial.print(DHT.humidity, 1);
-  Serial.print(",\t");
-  Serial.println(((DHT.temperature*(9.0/5.0))+32), 1);
+
+  tempC = DHT.temperature;
+  tempF = tempC * 9.0 / 5.0 + 32.0;
+  relH = DHT.humidity;
+
+  listenForEthernetClients();
 
   delay(2000);
 }
-//
-// END OF FILE
-//
+
+
+void listenForEthernetClients() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("Got a client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println();
+          // print the current readings, in HTML format:
+          client.print("Temperature (F): ");
+          client.print(tempF);
+          client.print(" degrees F");
+          client.println("<br />");
+          client.print("Temperature (C): ");
+          client.print(tempC);
+          client.print(" degrees C");
+          client.println("<br />");
+          client.print("Relative Humidity: " + String(relH));
+          client.print(" percent");
+          client.println("<br />");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+  }
+}
