2bfa335ffc6b0f2a1b3e31be7d5f391460c74820
Jonathan McFadden <mcfaddja@uw.edu>
2018-08-28 02:12:17 -0700

    added project 5 from CyberPhysicalSystems class as a reference

---
+++ 0-reference/project5/TCSS-596/Assets/Robot Kyle/Model/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
+
+public class ArduinoPlane : MonoBehaviour
+{
+    SerialPort serial = new SerialPort("COM6", 115200);
+    // Use this for initialization
+    void Start()
+    {
+        serial.Open();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!serial.IsOpen)
+        {
+            serial.Open();
+        }
+        string[] acc = serial.ReadLine().Split(',');
+        string[] gyr = serial.ReadLine().Split(',');
+        string[] mag = serial.ReadLine().Split(',');
+        Quaternion rot = Quaternion.Euler(new Vector3(float.Parse(gyr[0]), float.Parse(gyr[1]), float.Parse(gyr[2])));
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime * 2.0f);
+    }
+}
