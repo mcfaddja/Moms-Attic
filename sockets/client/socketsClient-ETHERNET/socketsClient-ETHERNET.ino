49eed3f7aeb0d601fac08965d93b8c8e4fb2ddb8
Jonathan McFadden <mcfaddja@uw.edu>
2018-08-28 09:17:15 -0700

    working client code

--- sockets/client/socketsClient-ETHERNET/socketsClient-ETHERNET.ino
+++ sockets/client/socketsClient-ETHERNET/socketsClient-ETHERNET.ino
@@ -1,31 +1,20 @@
 #include <dht.h>
 #include <Ethernet.h>
+#include <string.h>
 
 dht DHT;
 
 #define DHT11_PIN 2
 
 byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEC
 };
 IPAddress ip(192, 168, 1, 250);
 
-EthernetServer server(80);
-
-
-const char sensorName = "testEthernet1";
-
-
-double tempC = 0;
-double tempF = 0;
-double relH = 0;
-
-long lastReadingTime = 0;
-
+const char theSensorName[] = "testEthernet0";
 
 const uint16_t port = 5000;
 const char * host = "192.168.1.238";
-
 
 
 void setup() {
@@ -49,69 +38,37 @@ void setup() {
     Serial.println("Ethernet cable is not connected.");
   }
 
-  // start listening for clients
-  server.begin();
-
   delay(1000);
 }
 
 void loop() {
-  // check for a reading no more than once a second.
-  if (millis() - lastReadingTime > 5001) {
-    // if there's a reading ready, read it:
-    // don't do anything until the data ready pin is high:
-    // READ DATA
-    Serial.print("DHT11, \t");
-    int chk = DHT.read11(DHT11_PIN);
-    switch (chk)
-    {
-      case DHTLIB_OK:
-        Serial.println("OK,\t");
-        break;
-      case DHTLIB_ERROR_CHECKSUM:
-        Serial.print("Checksum error,\t");
-        break;
-      case DHTLIB_ERROR_TIMEOUT:
-        Serial.print("Time out error,\t");
-        break;
-      default:
-        Serial.print("Unknown error,\t");
-        break;
-    }
-
-    tempC = DHT.temperature;
-    tempF = tempC * 9.0 / 5.0 + 32.0;
-    relH = DHT.humidity;
-
-
+  // READ DATA
+  Serial.print("DHT11, \t");
+  int chk = DHT.read11(DHT11_PIN);
+  switch (chk)
+  {
+    case DHTLIB_OK:
+      Serial.println("OK,\t");
+      break;
+    case DHTLIB_ERROR_CHECKSUM:
+      Serial.print("Checksum error,\t");
+      break;
+    case DHTLIB_ERROR_TIMEOUT:
+      Serial.print("Time out error,\t");
+      break;
+    default:
+      Serial.print("Unknown error,\t");
+      break;
   }
 
-  doNetworkStuff();
+  double myRelH = DHT.humidity;
+  double myTempC = DHT.temperature;
+  double myTempF = ((DHT.temperature*(9.0/5.0))+32);
 
-  delay(99);
-}
+  Serial.println(myRelH);
+  Serial.println(myTempC);
+  Serial.println(myTempF);
 
-
-void doNetworkStuff() {
-  int theBuffSize = 640;
-  int theMiniBuffSize = 64;
-  char buff[theBuffSize];
-  char miniBuff[theMiniBuffSize];
-
-  memset(buff, '\0', sizeof(buff));
-  memset(miniBuff, '\0', sizeof(miniBuff));
-
-  snprintf(miniBuff, sizeof(miniBuff), "%s\r\n", sensorName);
-  strcat(buff, miniBuff);
-  
-  snprintf(miniBuff, sizeof(miniBuff), "%f\r\n", relH);
-  strcat(buff, miniBuff);
-
-  snprintf(miniBuff, sizeof(miniBuff), "%f\r\n", tempC);
-  strcat(buff, miniBuff);
-
-  snprintf(miniBuff, sizeof(miniBuff), "%f\r\n", tempF);
-  strcat(buff, miniBuff);
 
   EthernetClient client;
 
@@ -121,14 +78,18 @@ void doNetworkStuff() {
     delay(2500);
     return;
   }
-  
-  Serial.print("connected to");
+
+  Serial.print("connected to ");
   Serial.println(client.remoteIP());
 
-  client.println(buff);
+  client.println(theSensorName);
+  client.println(myRelH);
+  client.println(myTempC);
+  client.println(myTempF);
   Serial.println("data sent, closing connection...");
 
   client.stop();
   Serial.println("connection closed!!!");
-}
 
+  delay(2000);
+}
