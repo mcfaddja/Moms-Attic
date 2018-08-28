df364c6d5544d4765270490ffafb68109136543e
Jonathan McFadden <mcfaddja@uw.edu>
2018-08-27 18:18:06 -0700

    added stuff from the local Arduino directory on 'midget'

---
+++ Arduino/dht11_test/dht11_test.ino
@@ -0,0 +1,81 @@
+
+//
+//    FILE: dht11_test.ino
+//  AUTHOR: Rob Tillaart
+// VERSION: 0.1.01
+// PURPOSE: DHT library test sketch for DHT11 && Arduino
+//     URL:
+//
+// Released to the public domain
+//
+
+#include <dht.h>
+
+dht DHT;
+
+#define DHT11_PIN 16
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("DHT TEST PROGRAM ");
+  Serial.print("LIBRARY VERSION: ");
+  Serial.println(DHT_LIB_VERSION);
+  Serial.println();
+  Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (F),\tTemperature(C)");
+}
+
+
+int myCnt = 0;
+
+void loop()
+{
+  // READ DATA
+  Serial.print("DHT11, \t");
+  int chk = DHT.read11(DHT11_PIN);
+  switch (chk)
+  {
+    case DHTLIB_OK:  
+		Serial.print("OK,\t"); 
+		break;
+    case DHTLIB_ERROR_CHECKSUM: 
+		Serial.print("Checksum error,\t"); 
+		break;
+    case DHTLIB_ERROR_TIMEOUT: 
+		Serial.print("Time out error,\t"); 
+		break;
+    case DHTLIB_ERROR_CONNECT:
+        Serial.print("Connect error,\t");
+        break;
+    case DHTLIB_ERROR_ACK_L:
+        Serial.print("Ack Low error,\t");
+        break;
+    case DHTLIB_ERROR_ACK_H:
+        Serial.print("Ack High error,\t");
+        break;
+    default: 
+		Serial.print("Unknown error,\t"); 
+		break;
+  }
+  // DISPLAY DATA
+  Serial.print(DHT.humidity, 1);
+  Serial.print(",\t");
+  Serial.print("\t");
+//  Serial.print("\t");
+  Serial.print(((DHT.temperature*(9.0/5.0)) + 32.0), 1);
+  Serial.print(",\t");
+  Serial.print("\t");
+  Serial.print("\t");
+  Serial.println(DHT.temperature,1);
+
+  if(myCnt == 15) {
+    Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (F),\tTemperature(C)");
+    myCnt = 0;
+  } else {
+    myCnt++;
+  }
+  delay(2000);
+}
+//
+// END OF FILE
+//
