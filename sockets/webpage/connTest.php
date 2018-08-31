1c0167b2aec044c186f8397f532bedcde16a8468
Jonathan McFadden <mcfaddja@uw.edu>
2018-08-31 11:37:52 -0700

    corrected username and password

--- sockets/webpage/connTest.php
+++ sockets/webpage/connTest.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "jamster";
+$password = "1MckiD4";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
