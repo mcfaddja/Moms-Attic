945d7bb157f35ff42f243d7908d4cbfea75e937e
Jonathan McFadden <mcfaddja@uw.edu>
2018-08-31 11:33:39 -0700

    sensorID php query code

--- sockets/webpage/tempANDrelHwebpage.php
+++ sockets/webpage/tempANDrelHwebpage.php
@@ -6,15 +6,30 @@
 $servername = "localhost";
 $username = "jamster";
 $password = "1MckiD4";
+$dbname = "tempANDrelHsensors"
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 echo "Connected successfully";
+
+$sql = "SELECT DISTINCT sensorID from allTestSensors";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["sensorID"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
 ?>
 
 </body>
